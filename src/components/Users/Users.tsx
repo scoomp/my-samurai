7ed90846ea6fdc5fa031138f7fde0fd056fe1fb9
scoomp <moshnyakov.alexei@yandex.ru>
2023-12-17 15:07:40 +0300

    53.1 SAMURAI

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,6 +2,7 @@ import {UserType} from '../../redux/users-reducer';
 import styles from './Users.module.css'
 import axios from 'axios';
 import userPhoto from '../../assets/images/user.jpg'
+import React from 'react';
 
 type PropsType = {
     users: UserType[]
@@ -10,18 +11,19 @@ type PropsType = {
     setUsers: (users: UserType[]) => void
 }
 
-export const Users = (props: PropsType) => {
+class Users extends React.Component<PropsType> {
 
-    if (!props.users.length) {
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(res => {
-                props.setUsers(res.data.items)
+                this.props.setUsers(res.data.items)
             })
     }
 
-    return (
-        <div>
-            {props.users.map(u => <div key={u.id}>
+    render() {
+        return (
+            <div>
+                {this.props.users.map(u => <div key={u.id}>
           <span>
             <div>
               <img src={u.photos.small ? u.photos.small : userPhoto} alt="avatar" className={styles.userPhoto}/>
@@ -29,12 +31,12 @@ export const Users = (props: PropsType) => {
             <div>
               {
                   u.followed
-                      ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-                      : <button onClick={() => props.follow(u.id)}>Follow</button>
+                      ? <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button>
+                      : <button onClick={() => this.props.follow(u.id)}>Follow</button>
               }
             </div>
           </span>
-                <span>
+                    <span>
             <span>
               <div>{u.name}</div>
               <div>{u.status}</div>
@@ -44,7 +46,10 @@ export const Users = (props: PropsType) => {
               <div>{'u.location.city'}</div>
             </span>
           </span>
-            </div>)}
-        </div>
-    )
+                </div>)}
+            </div>
+        )
+    }
 }
+
+export default Users
